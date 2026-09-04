f889fbcb8a56af298fb8314899920fd50f62de9b
silvafael341-bit <silvafael341@gmail.com>
2026-09-04 06:56:15 -0300

    Ajusta wrapper do TranslatorOptions

--- app/src/main/java/com/manhuatranslator/MlKitTranslateCompat.kt
+++ app/src/main/java/com/manhuatranslator/MlKitTranslateCompat.kt
@@ -2,9 +2,28 @@ package com.google.mlkit.translate
 
 /** Compatibilidade para o namespace usado pelo código do projeto. */
 typealias Translator = com.google.mlkit.nl.translate.Translator
-typealias TranslatorOptions = com.google.mlkit.nl.translate.TranslatorOptions
+
+class TranslatorOptions internal constructor(
+    internal val delegate: com.google.mlkit.nl.translate.TranslatorOptions
+) {
+    class Builder {
+        private val delegate = com.google.mlkit.nl.translate.TranslatorOptions.Builder()
+
+        fun setSourceLanguage(language: String): Builder {
+            delegate.setSourceLanguage(language)
+            return this
+        }
+
+        fun setTargetLanguage(language: String): Builder {
+            delegate.setTargetLanguage(language)
+            return this
+        }
+
+        fun build(): TranslatorOptions = TranslatorOptions(delegate.build())
+    }
+}
 
 object Translation {
     fun getClient(options: TranslatorOptions): Translator =
-        com.google.mlkit.nl.translate.Translation.getClient(options)
+        com.google.mlkit.nl.translate.Translation.getClient(options.delegate)
 }
