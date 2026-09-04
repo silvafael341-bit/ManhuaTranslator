3cbcd38ad3556b712a8493c5b5b9adacd9652d4f
silvafael341-bit <silvafael341@gmail.com>
2026-09-04 06:51:01 -0300

    Bump versão para 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.manhuatranslator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.7.0"
+        versionCode = 8
+        versionName = "0.8.0"
     }
 
     compileOptions {
