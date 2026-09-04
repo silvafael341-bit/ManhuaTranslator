5c72f2d86124a9ba5346f566c69d42b1ebe57441
silvafael341-bit <silvafael341@gmail.com>
2026-09-04 07:42:01 -0300

    Atualiza versao para 0.8.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.manhuatranslator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.8.2"
+        versionCode = 11
+        versionName = "0.8.3"
     }
 
     compileOptions {
